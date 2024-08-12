1e14e0885a0d3fcf7a23cc981f0228c2fdde90db
gepinfo <info@geppettosoftware.com>
2024-08-12 10:43:14 +0000

    code commited from geppetto!

---
+++ application/client/web/tetsmani3/src/app/user/user.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { I18NextModule } from 'angular-i18next';
+import { UserComponent } from './user.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { ProfilesettingsComponent } from './profilesettings/profilesettings.component';
+import { ButtonRendererComponent } from './button-renderer/button-renderer.component';
+
+@NgModule({
+  imports: [
+AgGridModule,
+CommonModule,
+FormsModule,
+ReactiveFormsModule,
+RouterModule,
+I18NextModule.forRoot()
+],
+  declarations: [
+ProfilesettingsComponent,
+ButtonRendererComponent,
+UserComponent
+]
+  
+  
+})
+export class UserModule { }
---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_tetsmani3_7027: 'mongodb://admin:password@',
+    DB_COLL_tetsmani3_7027: ':27017/tetsmani3_7027?authSource=admin'
+}
